6c2f8f0aa05426c586bcf46ca318db7a0cfe79fc
brenda <138219828+brendiha@users.noreply.github.com>
2024-10-30 10:48:03 -0300

    ,

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function verificador(){
     const numero = document.getElementById('numero').value;
     const resultado = document.getElementById('resultado');
 
-    if(numero % 60 === 0){
-        resultado.textContent = "reprovado.";
+    if(numero % 2 === 0){
+        resultado.textContent = "Este número é par.";
     } else {
-        resultado.textContent = "aprovado.";
+        resultado.textContent = "Este número é ímpar.";
     }
 }
