7c6821fdb934d4c51c7cd10e8c908346d6f0e8e6
brenda <138219828+brendiha@users.noreply.github.com>
2024-11-05 06:52:58 -0300

    m

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function verificador(){
     const numero = document.getElementById('numero').value;
     const resultado = document.getElementById('resultado');
 
-    if(numero % 2 === 0){
+    if(numero % 60 === 50){
         resultado.textContent = "aprovado.";
     } else {
         resultado.textContent = "reprovado.";
