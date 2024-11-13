523f6bb1c1ce5a28b85f70d33b259f6982fa191f
brenda <138219828+brendiha@users.noreply.github.com>
2024-11-13 09:58:19 -0300

    v

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function verificador(){
+function verificador() {
     const numero1 = document.getElementById('numero').value;
     const numero2 = document.getElementById('numero').value;
     const numero3 = document.getElementById('numero').value;
     const resultado = document.getElementById('resultado');
-const media+ (parseFloat(numero1))+parseFloat(numero2)+parseFloat(numero3)/3;
-    if(media  >= 60){
+    const media= (parseFloat(numero1)) + parseFloat(numero2) + parseFloat(numero3) / 3;
+    if (media >= 60) {
         resultado.textContent = "aprovado.";
-    } else 
+    } else
         resultado.textContent = "reprovado.";
 }
